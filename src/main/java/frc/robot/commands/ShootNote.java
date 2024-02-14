64f5e71663e49ee187520893135c0bdaed107045
Jeff Tuttle <jefft138@gmail.com>
2024-02-13 20:10:34 -0500

    Use ArmToPositionWithEnd to fix new command groups
    Add ShooterAtSpeed to dashboard

--- src/main/java/frc/robot/commands/ShootNote.java
+++ src/main/java/frc/robot/commands/ShootNote.java
@@ -4,12 +4,10 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
-import frc.robot.commands.armCommands.ArmToPosition;
+import frc.robot.commands.armCommands.ArmToPositionWithEnd;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.ArmSubsystem.armPositions;
-import frc.robot.subsystems.IntakeSubsystem;
 
 public class ShootNote extends SequentialCommandGroup {
 
@@ -20,13 +18,13 @@ public class ShootNote extends SequentialCommandGroup {
 
         addCommands(
             new ParallelCommandGroup(
-                new ArmToPosition(armSubsystem, position), 
+                new ArmToPositionWithEnd(armSubsystem, position), 
                 new WarmUpShooter(shooterSubsystem)
             ),
             new RunCommand(()-> shooterSubsystem.transferOn(false), shooterSubsystem).withTimeout(1.0),
             new InstantCommand(()-> shooterSubsystem.shooterOff(), shooterSubsystem),
             new InstantCommand(()-> shooterSubsystem.transferOff(), shooterSubsystem),
-            new ArmToPosition(armSubsystem, armPositions.TRANSFER) //TODO This might need to be moved out for autos
+            new ArmToPositionWithEnd(armSubsystem, armPositions.TRANSFER) //TODO This might need to be moved out for autos
 
         ); 
     }
